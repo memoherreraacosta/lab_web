aae67e083a968dc50d3b1539da7a36594cb11c68
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-05-01 00:32:34 -0500

    Go files and files added
    Signed-off-by: Guillermo Herrera Acosta <a01400835@itesm.mx>

---
+++ ejercicios/song_quote.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Estrofa de la cancion "Bien por ti" de Costa de Ambar
+	fmt.Println(
+		"Bien por ti, bien por mi",
+		"que ya me he dado cuenta de las cosas ",
+		"y asi note que ya no eras tan hermosa, ",
+		"aun asi tienes poder sobre cualquiera que te mire,",
+		"lo se porque, ya lo he perdido todo por esos ojos...",
+	)
+}
